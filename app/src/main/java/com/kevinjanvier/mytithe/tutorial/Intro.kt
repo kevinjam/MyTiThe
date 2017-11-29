41f8edc75b6e575851e62ba0f7e7e2c745287ee3
Kevin Janvier <kevin.janvier5@gmail.com>
2017-11-29 19:50:25 +0300

    update kotlin

---
+++ app/src/main/java/com/kevinjanvier/mytithe/tutorial/Intro.kt
@@ -0,0 +1,14 @@
+package com.kevinjanvier.mytithe.tutorial
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kevinjanvier.mytithe.R
+
+class Intro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+    }
+}
